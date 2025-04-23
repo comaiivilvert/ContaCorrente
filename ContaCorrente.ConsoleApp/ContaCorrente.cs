52531ca228cafbd0052c2c05c5669fa967c4cd86
comaiivilvert <comaiivilvert@gmail.com>
2025-04-23 00:17:38 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ContaCorrente.ConsoleApp
+{ 
+    internal class ContaCorrente
+    {
+        public double saldo = 1000;
+        public int numero;
+        public double limite;
+        public Movimentacoes[] movimentacoes;
+        public int quantidadeMovimentacoes = 0;
+
+        public void Transferir(ContaCorrente destino, double valor)
+        {
+            bool conseguiuSacar = Sacar(valor);
+            if (conseguiuSacar)
+            {
+                destino.Depositar(valor);
+                AdicionarMovimentacao("Transferência para conta " + destino.numero, valor);
+            }
+        }
+        
+        public bool Sacar(double valor)
+        {
+            if (saldo + limite >= valor)
+            {
+                saldo -= valor;
+                AdicionarMovimentacao("Saque", valor);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+                return false;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            AdicionarMovimentacao("Deposito", valor);
+
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("Extrato da conta " + numero + ":");
+            for (int i = 0; i < quantidadeMovimentacoes; i++)
+            {
+                Console.WriteLine(movimentacoes[i].Descrever());
+            }
+            Console.WriteLine("Saldo atual: R$" + saldo);
+        }
+
+        public void AdicionarMovimentacao(string tipo, double valor)
+        {
+            if (quantidadeMovimentacoes < movimentacoes.Length)
+            {
+                movimentacoes[quantidadeMovimentacoes] = new Movimentacoes(tipo, valor);
+                quantidadeMovimentacoes++;
+            }
+            else
+            {
+                Console.WriteLine("Número máximo de movimentações atingido.");
+            }
+        }
+
+    }
+
+
+}
---
+++ ContaCorrente.ConsoleApp/Movimentacoes.cs
@@ -0,0 +1,18 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    class Movimentacoes
+    {
+        public string tipo;
+        public double valor;
+
+        public Movimentacoes(string t, double v)
+        {
+            tipo = t;
+            valor = v;
+        }
+        public string Descrever()
+        {
+            return tipo + ": R$" + valor;
+        }
+    }
+}
